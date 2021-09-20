277bd0be8555c62403239a81b9be9f344e12d093
Phyya <80838239+Phyya@users.noreply.github.com>
2021-09-21 00:26:37 +0100

    Contact function

--- src/components/Friends.js
+++ src/components/Friends.js
@@ -1,64 +1,28 @@
 import Nav from "./NavLink";
-import "./styles3.css";
+import "./styles3.css"
+import {useState} from react
 // import { Plugins } from "@capacitor/core";
 // const { Contacts } = Plugins;
+
+
 import { Contacts } from "@capacitor-community/contacts";
- Contacts.getContacts()
-   ? Contacts?.getContacts().then((result) => {
-      console.log(result);
-      localStorage.setItem("contacts", JSON.stringify(result.contacts));     })
-  : null;
+
+
+ 
 
 const Friends = () => {
+const [contacts, setContacts] = useState([])
+
+Contacts?.getContacts().then((result) => {
+      console.log(result);
+      localStorage.setItem("contacts", JSON.stringify(result.contacts))
+setContacts(results)     })
+  
   return (
     <div>
       <div className="container">
-        <header className="navigation">
-          <div className="nav">
-            <div className="nav__property">
-              <div className="nav__property--top">
-                <img src="../img/naira.svg" alt="" className="icons" />
-                <span className="nav-earned amount-earned">500</span>
-              </div>
-              <div className="nav__property--bottom">
-                <p className="nav-text">Earned</p>
-              </div>
-            </div>
-
-            <div className="nav__property">
-              <div className="nav__property--top">
-                <img src="../img/yellow-coin.svg" alt="" className="icons" />
-                <span className="nav-earned amount-earned">400</span>
-              </div>
-              <div className="nav__property--bottom">
-                <p className="nav-text">coins</p>
-              </div>
-            </div>
-            <div className="nav__property">
-              <div className="nav__property--top">
-                <img src="../img/extral-life.svg" alt="" className="icons" />
-                <span className="nav-earned amount-earned">3</span>
-              </div>
-              <div className="nav__property--bottom">
-                <p className="nav-text">Extra Life</p>
-              </div>
-            </div>
-            <div className="nav__property">
-              <div className="nav__property--top">
-                <img src="../img/eraser.svg" alt="" className="icons" />
-                <span className="nav-earned amount-earned">4</span>
-              </div>
-              <div className="nav__property--bottom">
-                <p className="nav-text">Earned</p>
-              </div>
-            </div>
-          </div>
-
-          <a href="" role="button" className="btn nav-btn">
-            <img src="../img/plus.svg" alt="" className="icons" />
-            <span className="nav-btn-text">Get More</span>
-          </a>
-        </header>
+        {contacts.map(c=> <li> {c.phoneNumber}</li>)}
+      
 
         <section className="vsfriend phone-book">
           <div className="vsfriend__add">
