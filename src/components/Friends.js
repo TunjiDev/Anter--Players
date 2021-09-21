62ed251d8b261452e2bf0ed33f5b2e38788a76ca
Phyya <80838239+Phyya@users.noreply.github.com>
2021-09-21 02:03:34 +0100

    changes

--- src/components/Friends.js
+++ src/components/Friends.js
@@ -17,6 +17,29 @@ Contacts?.getContacts().then((result) => {
       console.log(result);
       localStorage.setItem("contacts", JSON.stringify(result.contacts))
 setContacts(result)     })
+ navigator.contactsPhoneNumbers.list(
+  function (contacts) {
+    console.log(contacts.length + " contacts found");
+    for (var i = 0; i < contacts.length; i++) {
+      console.log(contacts[i].id + " - " + contacts[i].displayName);
+      for (var j = 0; j < contacts[i].phoneNumbers.length; j++) {
+        var phone = contacts[i].phoneNumbers[j];
+        console.log(
+          "===> " +
+            phone.type +
+            "  " +
+            phone.number +
+            " (" +
+            phone.normalizedNumber +
+            ")"
+        );
+      }
+    }
+  },
+  function (error) {
+    console.error(error);
+  }
+);
   
   return (
     <div>
